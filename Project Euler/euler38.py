60c7a2cf5ab4993bd154bbc63bc8730c64ccf327
Kshitij Dawar <49729005+kdawar1@users.noreply.github.com>
2020-10-13 14:11:59 -0400

    create euler38.py

---
+++ Project Euler/euler38.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+def largest_pandigital():
+    ans = 0
+    for n in range(2, 10):
+        for i in range(1, 10**(9 // n)):
+            s = "".join(str(i * j) for j in range(1, n + 1))
+            if "".join(sorted(s)) =='123456789':
+                ans = max(int(s),ans)                    
+    return ans
+
+
+if __name__ == "__main__":
+	print(largest_pandigital())
